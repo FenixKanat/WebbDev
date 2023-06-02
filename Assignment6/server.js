61943c288b4bd384b4d5d576bbf35be1b04edd17
roseygirlforra <gulcinkanat97@gmail.com>
2023-06-02 15:23:00 +0200

    port

--- Assignment6/server.js
+++ Assignment6/server.js
@@ -2,17 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const port = 3000; // Choose a port number for your server
 
-// Serve static files from the 'Assignment6' directory
+
 app.use(express.static(path.join(__dirname)));
 
-// Handle requests for the HTML file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'firstPage.html'));
 });
 
-// Start the server
+const port = process.env.PORT || 3000; 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
